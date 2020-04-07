09267efcbb221481a4409ef5492756d234b09845
suzukikarol <karol.suzuki.23@gmail.com>
2020-04-07 00:44:29 -0300

    importando component em routes e adicionando rota ao component pai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SobreTypeComponent } from './sobre-type/sobre-type.component';
+import { FirstStepComponent } from './first-step/first-step.component';
 
 const routes: Routes = [
-  {path:"sobre", component:SobreTypeComponent}
+  {path:"sobre", component:SobreTypeComponent},
+  {path:"first", component:FirstStepComponent}
 ];
 
 @NgModule({
